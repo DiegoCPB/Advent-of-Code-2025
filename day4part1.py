d468279b4fc938999eaa159a83ae7298b9e1e845
DiegoCPB <diego.chou@gmail.com>
2025-12-04 21:20:56 +0100

    day4 challenge

---
+++ day4part1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025-12-04 20:37:55 UTC+01:00
+
+@author: DiegoCPB
+"""
+
+import numpy as np
+from scipy import signal
+
+def count_rolls(filepath):
+    k = np.array([[1,1,1],[1,0,1],[1,1,1]])
+    with open(filepath,'r') as file:
+        M = np.array([list(line.rstrip()) for line in file if line.rstrip()])
+    M = (M=='@').astype(int)
+    conv = signal.convolve2d(M,k,mode='same')
+    res = (conv<4)*M
+    return np.sum(res)
+
+if __name__ == "__main__":
+    filepath = 'inputs/day4.txt'
+    print(count_rolls(filepath))
---
+++ day4part2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025-12-04 20:37:55 UTC+01:00
+
+@author: DiegoCPB
+"""
+
+import numpy as np
+from scipy import signal
+
+def count_rolls(filepath):
+    k = np.array([[1,1,1],[1,0,1],[1,1,1]])
+    with open(filepath,'r') as file:
+        M = np.array([list(line.rstrip()) for line in file if line.rstrip()])
+    M = (M=='@').astype(int)
+    R = np.zeros(M.shape)
+    while True:
+        conv = signal.convolve2d(M,k,mode='same')
+        Ri = (conv<4)*M
+        if np.any(Ri!=0):
+            R += Ri
+            M -= Ri
+        else:
+            break
+    return np.sum(R)
+
+if __name__ == "__main__":
+    filepath = 'inputs/day4.txt'
+    print(count_rolls(filepath))
