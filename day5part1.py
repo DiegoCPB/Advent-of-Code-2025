af428a709328802a469b96c7b8cef1c277ee883d
DiegoCPB <diego.chou@gmail.com>
2025-12-05 19:12:41 +0100

    day5 parts 1 and 2

---
+++ day5part1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025-12-05 18:53:18 UTC+01:00
+
+@author: DiegoCPB
+"""
+
+def fresh_ingredients(filepath):
+    rngs = []
+    IDs = []
+    with open(filepath) as file:
+        read_rngs = True
+        for line in file:
+            if not line.rstrip():
+                read_rngs = False
+            elif read_rngs:
+                rng = [int(i) for i in line.split('-')]
+                rngs.append(rng)
+            else:
+                IDs.append(int(line))
+    func = lambda x: any([(x>=rng[0])*(x<=rng[1]) for rng in rngs])
+    counter = 0
+    for ID in IDs:
+        counter += func(ID)
+    return counter
+
+if __name__ == "__main__":
+    filepath = 'inputs/day5.txt'
+    print(fresh_ingredients(filepath))
---
+++ day5part2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025-12-05 18:53:13 UTC+01:00
+
+@author: DiegoCPB
+"""
+
+def fresh_ingredients(filepath):
+    rngs = []
+    vals = []
+    with open(filepath) as file:
+        read_rngs = True
+        for line in file:
+            if not line.rstrip():
+                read_rngs = False
+            elif read_rngs:
+                rng = [int(i) for i in line.split('-')]
+                rngs.append(rng)
+                vals += rng
+    vals = sorted(list(set(vals)))
+    func = lambda x: any([(x>=rng[0])*(x<=rng[1]) for rng in rngs])
+    counter = 0
+    for i in range(len(vals)-1):
+        mean = 0.5*(vals[i]+vals[i+1])
+        n_between = vals[i+1]-vals[i]-1
+        counter += func(mean)*n_between+2
+    return counter-(len(vals)-2)
+
+if __name__ == "__main__":
+    filepath = 'inputs/day5.txt'
+    print(fresh_ingredients(filepath))
