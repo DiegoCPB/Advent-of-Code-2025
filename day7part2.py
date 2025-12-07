0a966be9814ce9b8fd7ba317b6c098a76dc5cc3e
DiegoCPB <diego.chou@gmail.com>
2025-12-07 13:33:17 +0100

    remove counter

--- day7part2.py
+++ day7part2.py
@@ -8,7 +8,6 @@ Created on 2025-12-07 12:23:17 UTC+01:00
 def beam_splits(filepath):
     with open(filepath) as file:
         table = [[-1 if char=='^' else 0 if char=='.' else 1 for char in line.strip('\n')] for line in file]
-    counter = 0
     for i in range(1,len(table)):
         for j in range(len(table[0])):
             if table[i][j] >= 0:
