8707533837a6f577eb01565cba196037574090bd
DiegoCPB <diego.chou@gmail.com>
2025-12-13 00:47:52 +0100

    day 11 count paths topo

--- 2025/day11part1.py
+++ 2025/day11part1.py
@@ -5,20 +5,20 @@ Created on 2025-12-12 20:39:46 UTC+01:00
 @author: DiegoCPB
 """
 
+def count_paths(links,node,end):
+    if node == end:
+        return 1
+    return sum(count_paths(links,n,end) for n in links[node])
+
 def you2out(filepath):
-    def expand_node(node):
-        if node == 'out':
-            return node
-        else:
-            return [expand_node(n) for n in links[node]]
     links = {}
     with open(filepath) as file:
         for line in file:
             if line.rstrip():
                 words = line.split()
                 links[words[0][:-1]] = words[1:]
-    res = str(expand_node('you'))
-    return len(res.split('out'))-1
+    res = count_paths(links,'you','out')
+    return res
 
 if __name__ == "__main__":
     filepath = 'inputs/day11.txt'
---
+++ 2025/day11part2.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025-12-12 20:58:02 UTC+01:00
+
+@author: DiegoCPB
+"""
+
+from collections import defaultdict, deque
+
+def count_paths_topo(graph, start, end):
+    indegree = defaultdict(int)
+    for u in graph:
+        for v in graph[u]:
+            indegree[v] += 1
+    q = deque([u for u in graph if indegree[u] == 0])
+    topo = []
+    while q:
+        u = q.popleft()
+        topo.append(u)
+        for v in graph.get(u, []):
+            indegree[v] -= 1
+            if indegree[v] == 0:
+                q.append(v)
+    dp = defaultdict(int)
+    dp[start] = 1
+    for u in topo:
+        for v in graph.get(u, []):
+            dp[v] += dp[u]
+    return dp[end]
+
+def svr2out(filepath):
+    links = {}
+    res = 1
+    with open(filepath) as file:
+        for line in file:
+            if line.rstrip():
+                words = line.split()
+                links[words[0][:-1]] = words[1:]
+    res *= count_paths_topo(links,'svr','fft')
+    res *= count_paths_topo(links,'fft','dac')
+    res *= count_paths_topo(links,'dac','out')
+    return res
+
+if __name__ == "__main__":
+    filepath = 'inputs/day11.txt'
+    print(svr2out(filepath))
