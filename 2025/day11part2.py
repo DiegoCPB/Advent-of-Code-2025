f40742e2f7bccbbd884b00397bd1316bd6d614d9
DiegoCPB <diego.chou@gmail.com>
2025-12-20 00:00:47 +0100

    day 11 part 2 removed deque

--- 2025/day11part2.py
+++ 2025/day11part2.py
@@ -5,17 +5,17 @@ Created on 2025-12-12 20:58:02 UTC+01:00
 @author: DiegoCPB
 """
 
-from collections import defaultdict, deque
+from collections import defaultdict
 
 def count_paths_topo(graph, start, end):
     indegree = defaultdict(int)
     for u in graph:
         for v in graph[u]:
             indegree[v] += 1
-    q = deque([u for u in graph if indegree[u] == 0])
+    q = [u for u in graph if indegree[u] == 0]
     topo = []
     while q:
-        u = q.popleft()
+        u = q.pop(0)
         topo.append(u)
         for v in graph.get(u, []):
             indegree[v] -= 1
